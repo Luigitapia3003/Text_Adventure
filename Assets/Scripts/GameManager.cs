de888e0eb7ce1ea3357ad451af5b3d5c6590ef51
Luigo3003 <joslutaes@gmail.com>
2022-02-09 11:42:27 -0600

    Se agregó el game manager y la lista de palabras y preguntas

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]  string[] palabrasGuardadas;
+    [SerializeField]  string[] preguntas;
+    [SerializeField]  string historia;
+
+    [SerializeField] TextMeshProUGUI textoPreguntas;
+    [SerializeField] TextMeshProUGUI textoHistoria;
+    [SerializeField] TMP_InputField inputRespuesta;
+    [SerializeField] GameObject botonRespuesta;
+
+    int indicePregunta = 0;
+    void Start()
+    {
+        textoPreguntas.text = preguntas [indicePregunta];
+        palabrasGuardadas = new string[preguntas.Length];
+
+    }
+
+    public void GuardarRespuesta()
+    {
+        // Guardar la respuesta del jugador
+        palabrasGuardadas[indicePregunta] = inputRespuesta.text;
+        
+        // Limpiar la respuesta 
+        inputRespuesta.text = "";
+
+        //Poner la siguiente pregunta 
+        indicePregunta++;
+
+        if (indicePregunta >= preguntas.Length)
+        {
+            MostrarHistoria();
+        }
+
+        else
+        {
+            textoPreguntas.text = preguntas[indicePregunta];
+        }
+    }
+
+    void MostrarHistoria()
+    {
+        textoHistoria.gameObject.SetActive(true);
+        textoHistoria.text = historia;
+
+        textoPreguntas.gameObject.SetActive(false);
+        botonRespuesta.SetActive(false);
+        inputRespuesta.gameObject.SetActive(false);
+
+
+    }
+}
